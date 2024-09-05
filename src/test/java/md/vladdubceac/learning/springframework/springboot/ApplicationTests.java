f2a57698fa337eff2817debcf695b413b8217701
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-09-05 23:02:10 +0300

    First commit

---
+++ src/test/java/md/vladdubceac/learning/springframework/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package md.vladdubceac.learning.springframework.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
